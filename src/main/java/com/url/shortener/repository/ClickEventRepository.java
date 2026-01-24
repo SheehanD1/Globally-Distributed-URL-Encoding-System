6bcd3267a86cf246dd93e5ebd0150b45a31c9e9e
SheehanD1 <sheehandandapat@gmail.com>
2026-01-23 23:01:09 -0500

    Added auth logic

--- src/main/java/com/url/shortener/repository/ClickEventRepository.java
+++ src/main/java/com/url/shortener/repository/ClickEventRepository.java
@@ -1,7 +1,6 @@
 package com.url.shortener.repository;
 
 import com.url.shortener.models.ClickEvent;
-import com.url.shortener.repository.UrlMappingRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
